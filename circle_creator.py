0afd305defcf5412f894199b8934222e5778d3f3
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-22 18:29:05 -0600

    Update circle_creator.py

--- circle_creator.py
+++ circle_creator.py
@@ -11,19 +11,19 @@ num_images = 10
 
 for i in range(num_images):
     # Create a new image with white background
-    img = Image.new('RGB', image_size, 'white')
+    img = Image.new('L', image_size, 'white')
     draw = ImageDraw.Draw(img)
 
     # Generate random coordinates for the circle's center
     x = random.randint(0, image_size[0])
     y = random.randint(0, image_size[1])
     # Generate a random radius
-    radius = random.randint(10, 100)
+    radius = random.randint(1, 100)
 
     # Draw the circle
     left_up_point = (x-radius, y-radius)
     right_down_point = (x+radius, y+radius)
-    draw.ellipse([left_up_point, right_down_point], outline='black', fill='blue')
+    draw.ellipse([left_up_point, right_down_point], outline='black', fill='black')
 
     # Save the image
     img.save(f'photos/circle_{i+1}.png')
