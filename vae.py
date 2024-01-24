8704b5e9bc9ed15a0980bfdaa6c63d2bffb2ee60
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-23 22:41:09 -0600

    Update vae.py

--- vae.py
+++ vae.py
@@ -110,14 +110,14 @@ transform = transforms.Compose([
 dataset = CustomDataset(folder_path='photos', transform=transform)
 
 # Dataset and Dataloader
-dataloader = DataLoader(dataset, batch_size=400, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=200, shuffle=True)
 
 # Instantiate VAE model with latent_dim
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = VariationalAutoencoder(latent_dim=LATENT_DIM).to(device)
 
 # Loss and optimizer
-optimizer = optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.00, amsgrad=False)
+optimizer = optim.Adam(model.parameters(), lr=0.00001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.00, amsgrad=False)
 
 # Train the model
 num_epochs = 100000
